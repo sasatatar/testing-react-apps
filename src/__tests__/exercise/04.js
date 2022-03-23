55998fa618cfd387739cd1a6cd5942eee7d4a4c9
Sasha Tatar <sasa.tatar@gmail.com>
2022-03-23 14:59:55 +0100

    Use test data bot

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -5,15 +5,14 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
-import faker from 'faker';
+import { build, fake } from '@jackfranklin/test-data-bot';
 
-function buildLoginForm(config) {
-  return {
-    username: faker.internet.userName(),
-    password: faker.internet.password(),
-    ...config
+const buildLoginForm = build({
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password())
   }
-}
+});
 
 test('submitting the form calls onSubmit with username and password', () => {
   const handleSubmit = jest.fn();
